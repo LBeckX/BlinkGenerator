c0ef13f06a9380ae53077eb0a27acd22099c7abf
LBeckX <lb@1601.com>
2017-08-18 20:10:56 +0200

    Init commit

---
+++ htdocs/files/js/generate.js
@@ -0,0 +1,112 @@
+var outputCSS = "";
+var outputHtml = "";
+var speedInMs = "2000";
+var colorOne = "212121";
+var colorTwo = "00FFB8";
+var inputElementSpeed,
+    inputElementColorOne,
+    inputElementColorTwo,
+    outputTextAreaCSS,
+    outputTextAreaHTML,
+    outputTextMSG;
+
+document.addEventListener("DOMContentLoaded", function () {
+    inputElementSpeed=document.getElementById("INPUT_SPEED");
+    inputElementColorOne=document.getElementById("INPUT_COLOR_ONE");
+    inputElementColorTwo=document.getElementById("INPUT_COLOR_TWO");
+    outputTextAreaCSS=document.getElementById("OUTPUT_CSS_CODE");
+    outputTextAreaHTML=document.getElementById("OUTPUT_HTML_CODE");
+    outputTextMSG = document.getElementById("OUTPUT_MSG");
+
+    var inputElements = document.getElementsByTagName("input");
+    for (var i=0;i<inputElements.length;i++){
+        inputElements[i].addEventListener("change",addModifiedInput);
+    }
+
+    outputTextAreaCSS.addEventListener("click",copyToClipboard);
+    outputTextAreaHTML.addEventListener("click",copyToClipboard);
+
+    addModifiedInput();
+}, false);
+
+
+function addModifiedInput(){
+    switch(this.name){
+        case "speed":
+            speedInMs = this.value;
+            break;
+        case "color1":
+            colorOne=this.value;
+            break;
+        case "color2":
+            colorTwo=this.value;
+            break;
+    }
+    updateFormInputs();
+    upadteOutputText();
+
+    return false;
+}
+
+function updateFormInputs() {
+    inputElementSpeed.value = speedInMs;
+    inputElementColorOne.value = colorOne;
+    inputElementColorTwo.value = colorTwo;
+}
+
+function upadteOutputText() {
+    outputCSS = '@-webkit-keyframes glowblink{\n' +
+        '0%,100%{background-color: #'+colorOne+';}\n' +
+        '50%{background-color: #'+colorTwo+';}\n' +
+        '}\n' +
+        '@keyframes glowblink{\n' +
+        '0%,100%{background-color: #'+colorOne+';}\n' +
+        '50%{background-color: #'+colorTwo+';}\n' +
+        '}\n' +
+
+        '#GLOWBLINK_ELEMENT{\n' +
+        'animation-name:glowblink;\n' +
+        'animation-duration:'+(speedInMs/1000)+'s;\n' +
+        'animation-iteration-count:infinite;"\n' +
+        '}\n';
+
+    outputHtml = '' +
+        '<div id="GLOWBLINK_ELEMENT">' +
+        '</div>';
+
+    document.getElementsByTagName('style')[0].innerHTML = outputCSS;
+
+    outputTextAreaCSS.innerText = outputCSS;
+    outputTextAreaHTML.innerText = outputHtml;
+
+}
+
+function copyToClipboard() {
+    this.focus();
+    this.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        var textMSG = 'Copying to clipboard was ' + msg;
+        addRemoveClass(outputTextMSG,"open",2000);
+        outputTextMSG.innerText = textMSG;
+    } catch (err) {
+        outputTextMSG.innerText = 'Oops, unable to copy';
+    }
+}
+
+
+function addClass(element,className) {
+    var a = element;
+    className = ' '+className;
+    element.className = element.className.replace( className, '' );
+    element.className = element.className + className;
+}
+
+function addRemoveClass(element,className,timeInMs) {
+    addClass(element,className);
+    setTimeout(function () {
+        element.className = element.className.replace(className,'');
+    },timeInMs)
+}
---
+++ htdocs/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>GlowBlink Generator</title>
+    <meta name="description" content="Erstelle Dir automatisch einen Blinkeffekt (CSS)">
+    <meta charset="utf-8">
+    <meta name="author" content="Lukas Beck <lb@1601.com>">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i" rel="stylesheet">
+    <link rel="stylesheet" href="files/css/design.css">
+    <script src="files/js/generate.js" type="text/javascript"></script>
+    <style></style>
+</head>
+<body>
+<div id="LEFT">
+    <div id="GLOWBLINK_BOX">
+        <div id="GLOWBLINK_ELEMENT"></div>
+    </div>
+</div>
+
+<div id="RIGHT">
+    <form id="GENERATOR_FORM">
+        <div class="form_input">
+            <label for="INPUT_SPEED">Speed in ms</label>
+            <input type="text" name="speed" id="INPUT_SPEED" placeholder="2000" minlength="3" maxlength="6">
+        </div>
+
+        <div class="form_input">
+            <label for="INPUT_COLOR_ONE">Color One #</label>
+            <input type="text" name="color1" id="INPUT_COLOR_ONE" placeholder="FFFFFF" minlength="3" maxlength="6">
+        </div>
+
+        <div class="form_input">
+            <label for="INPUT_COLOR_TWO">Color Two #</label>
+            <input type="text" name="color2" id="INPUT_COLOR_TWO" placeholder="BBBBBB" minlength="3" maxlength="6">
+        </div>
+
+        <div class="form_output">
+            <label for="OUTPUT_CSS_CODE">Output CSS Code</label>
+            <textarea id="OUTPUT_CSS_CODE" readonly="readonly"></textarea>
+        </div>
+        <div class="form_output">
+            <label for="OUTPUT_HTML_CODE">Output HTML Code</label>
+            <textarea id="OUTPUT_HTML_CODE" readonly="readonly"></textarea>
+        </div>
+        <div id="OUTPUT_MSG"></div>
+    </form>
+</div>
+</body>
+</html>
